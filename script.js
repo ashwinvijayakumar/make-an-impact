1f47723a084bbe6858f5c5b8c0927ee2e6e1a8be
Ashwin Vijayakumar <ashwinvijayakumar@gmail.com>
2025-05-07 09:46:13 -0700

    Update script.js
    Tweaked multiple impact line items.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const events = [
-  { year: '2006', text: 'Founded a rapid prototyping consulting firm' },
-  { year: '2007', text: 'Two engineers on payroll, achieved cash-flow positive' },
-  { year: '2008', text: 'Unable to scale, folded company' },
-  { year: '2012', text: "Led a global team for world's first Radar-based Blindspot Indicator (BLIS)" },
-  { year: '2014', text: "Catalyzed a new robotics business unit at Qualcomm" },
-  { year: '2017', text: "Founding member of Intel Movidius Neural Compute Stick (NCS), built developer.movidius.com" },
-  { year: '2019', text: "Product managed 3 generations of Intel Movidius discrete NPU PCIe cards" },
-  { year: '2022', text: "Incubated NUCaaS inside Intel CCG, deployed in 13 real-world cases, OM improved to 33%" },
-  { year: '2023', text: "$30M opportunity recorded on Salesforce; only $1M realized before NUC-G spin-off to Asus" },
-  { year: '2024', text: "Transitioned cloud engagements to Intel Tiber Infrastructure Manager" },
-  { year: '20xx', text: "Gain GM experience by running a PnL within an enterprise company" },
-  { year: '20xx', text: "Back to startup grassroots! This time, plan for scale!" }
+  { year: '2006 - Entreprenuer @ Afflatus Techtronics', text: 'Founded a rapid prototyping consulting startup' },
+  { year: '2007 - Entreprenuer @ Afflatus Techtronics', text: 'Achieved cash-flow positive | Added two employees on payroll' },
+  { year: '2008 - Entreprenuer @ Afflatus Techtronics', text: 'Unable to scale, folded my startup' },
+  { year: '2012 - Software Engineering Lead @ Delphi Automotive Electronics & Safety', text: "Led a global software team for world's first Radar-based Blindspot Indicator (Early ADAS)" },
+  { year: '2014 - Engineer @ Qualcomm', text: "Catalyzed a new robotics business unit at Qualcomm" },
+  { year: '2017 - Developer Evangelist @ Intel Movidius', text: "Founding member of Intel Movidius Neural Compute Stick (NCS), built developer.movidius.com" },
+  { year: '2019 - Product Manager @ Intel IoTG/VMC', text: "Lead the development and scale of 3 generations of Intel Movidius discrete NPU PCIe cards" },
+  { year: '2022 - Product Manager @ Intel CCG/NUC-G', text: "Incubated NUCaaS inside Intel CCG, deployed in 13 real-world cases, improved NUC operating margin to 33%" },
+  { year: '2023 - Product Manager @ Intel CCG/NUC-G', text: "$30M opportunity recorded on Salesforce | Only $1M realized before NUC-G spin-off to Asus" },
+  { year: '2024 - Product Manager @ Intel NEX', text: "Transitioned AWS, Azure and RedHat engagements from NUCaaS to Intel Tiber Infrastructure Manager" },
+  { year: '20xx - General Manager @ TDB Enterprise', text: "Gain GM experience by running a PnL within an enterprise company" },
+  { year: '20xx - Entreprenuer @ Stealth Startup', text: "Back to startup grassroots! This time, plan for scale!" }
 ];
 
 const timeline = document.querySelector('.timeline');
